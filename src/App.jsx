57b24570f6d89fa93d7010ce0ac3fb787ff779ac
Mohamed Gamal <mohamedgamalfahmy1@gamil.com>
2025-08-19 04:52:59 +0300

    First commit - Ecommerce project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+
+const App = () => {
+  return (
+<div className="flex items-center justify-center h-screen bg-gray-900">
+  <h1 className="text-9xl text-white">hello</h1>
+</div>
+  )
+}
+
+export default App
